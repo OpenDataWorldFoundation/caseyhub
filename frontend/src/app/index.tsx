421a51a9cde25a1a150ab35fb29fdf325dd0c029
prod-chirag <chiragluitel32@gmail.com>
2026-04-16 13:01:21 +1000

    v1.0.2

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-3xl font-bold text-red-500">Welcome to Casey Hub v1.0.1.</Text>
     </View>
   );
 }
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/app/**/*.{js,jsx,ts,tsx}",          
+    "./src/components/**/*.{js,jsx,ts,tsx}",   
+    "./src/types/**/*.{js,jsx,ts,tsx}"         
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
