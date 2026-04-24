a4515bf7453263194ac4a9f745507e02a14fb882
prod-chirag <chiragluitel32@gmail.com>
2026-04-24 15:05:14 +1000

    register

--- frontend/src/app/(tabs)/Profile/Register.tsx
+++ frontend/src/app/(tabs)/Profile/Register.tsx
@@ -4,21 +4,21 @@ import { Pressable, TextInput, View } from "react-native"
 const Register = () => {
     return (
         <View className="flex-1 justify-center items-center px-6">
-                <TextInput
-                    placeholder="Full Name"
-                    className="w-full border border-black-700 rounded-lg p-4"
-                />
-                <TextInput
-                    placeholder="Username"
-                    className="w-full border border-black-700 rounded-lg p-4"
-                />
-                <TextInput
-                    placeholder="Password"
-                    secureTextEntry
-                    className="w-full border border-black-700 rounded-lg p-4"
-                />
-                <CustomButtonComponent label="Register" onClick={()=>console.log('Register!')} /> 
-            </View>
+            <TextInput
+                placeholder="Full Name"
+                className="w-full border border-black-700 rounded-lg p-4"
+            />
+            <TextInput
+                placeholder="Username"
+                className="w-full border border-black-700 rounded-lg p-4"
+            />
+            <TextInput
+                placeholder="Password"
+                secureTextEntry
+                className="w-full border border-black-700 rounded-lg p-4"
+            />
+            <CustomButtonComponent label="Register" onClick={()=>console.log('Register!')} /> 
+        </View>
     )
 }
 
